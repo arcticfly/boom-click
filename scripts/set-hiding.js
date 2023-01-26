b31f5fb7ceee888b356a2ea6a7963ea7f88b4e2d
David Corbitt <david@realityplatforms.org>
2023-01-25 20:41:44 -0800

    Add better highlighting, fix link navigation bug

--- scripts/set-hiding.js
+++ scripts/set-hiding.js
@@ -3,9 +3,9 @@ let hiddenElements = [];
 // determine which element was interacted with in this event
 function getTargetFromEvent(e) {
   let target;
-  if (!e) e = window.event;
+  if (!e) e = document.event;
   if (e.target) target = e.target;
-  else if (e.srcElement) targ = e.srcElement;
+  else if (e.srcElement) target = e.srcElement;
   if (target.nodeType == 3)
     // defeat Safari bug
     target = target.parentNode;
@@ -14,59 +14,79 @@ function getTargetFromEvent(e) {
 
 // keep track of the previous background color for the most recently highlighted element
 let prevBackground;
-// keep track of which element was highlighted most recently
-let prevHighlightedElement;
+// keep track of currently highlighted element
+let highlightedElement;
+// keep track of colored highlight div so we can remove it later
+let highlight;
+// keep track of the highlighted element's previous click listener so we can restore it when the element is no longer highlighted
+let prevOnClick;
 
 // Update which element is highlighted and restore the previous element's background color, if another element was highlighted before
 function updateHighlight(event) {
   let element = getTargetFromEvent(event);
   // skip highlighting if this is not an element whose background we can change
-  if (!element.style) {
-    return
+  if (!element?.getBoundingClientRect) {
+    return;
   }
-  if (prevHighlightedElement) {
-    unhighlight(prevHighlightedElement);
-  }
-  // record prevHighlightedElement to make sure it is unhighlighted when mouse moves over something else
-  prevHighlightedElement = element;
-  // record prevBackground so we can restore the background color after we're finished highlighting
-  prevBackground = element.style.background;
-  element.style.background = "lightblue";
+  removeHighlight();
+
+  const rect = element.getBoundingClientRect();
+  const {height, width, left, top} = rect;
+
+  // record highlightedElement to make sure it is unhighlighted when mouse moves over something else
+  highlightedElement = element;
+  prevOnClick = highlightedElement.onclick
+  highlightedElement.onclick = (event) => hide(event)
+
+  // show highlight on the screen
+  highlight = document.createElement("div");
+  highlight.style.position = "absolute";
+  highlight.style.top = top + window.scrollY + "px";
+  highlight.style.left = left + window.scrollX + "px";
+  highlight.style.width = width + "px";
+  highlight.style.height = height + "px";
+  highlight.style.backgroundColor = "orange";
+  highlight.style.opacity = "50%";
+  highlight.style.pointerEvents = "none";
+  highlight.style.zIndex = 1000;
+  document.body.appendChild(highlight);
 }
 
-// restore previous background color to element
-function unhighlight(element) {
-  element.style.background = prevBackground;
-  prevBackground = null;
+// remove any highlight and restore the original onclick functionality of the highlighted element
+function removeHighlight() {
+  if (!highlightedElement) return // only remove highlight if there was one to begin with
+  highlight.remove()
+  highlightedElement.onclick = prevOnClick;
+  prevOnClick = null;
 }
 
 // while we are in hiding mode, we should highlight the element that clicking would hide
 function startHighlighting() {
-  document.addEventListener("mouseenter", updateHighlight, { capture: true });
+  document.addEventListener("mouseover", updateHighlight, { capture: true });
 }
 
 // after exiting hiding mode, we should stop highlighting elements
 function stopHighlighting() {
-  document.removeEventListener("mouseenter", updateHighlight, {
+  document.removeEventListener("mouseover", updateHighlight, {
     capture: true,
   });
-  if (prevHighlightedElement) {
-    // ensure that any element highlighted at the time is no longer highlighted
-    unhighlight(prevHighlightedElement);
-  }
+  // ensure that any element highlighted at the time is no longer highlighted
+  removeHighlight()
 }
 
 // determine which element was clicked and set its display to "none"
 function hide(event) {
-  let element = getTargetFromEvent(event);
-  const prevDisplay = element.style.display;
-  element.style.display = "none";
-  hiddenElements.push({ element, prevDisplay });
+  const prevDisplay = highlightedElement.style.display;
+  highlightedElement.style.display = "none";
+  hiddenElements.push({ element: highlightedElement, prevDisplay });
+  // Prevent user from navigating while deleting a link from the page
+  event.preventDefault();
+  event.stopImmediatePropagation();
 }
 
 // restore previous display settings to the most recently hidden element
 function redisplayPrevItem() {
-  if (!hiddenElements.length) return // we can't display an element that isn't hidden
+  if (!hiddenElements.length) return; // we can't display an element that isn't hidden
   const { element, prevDisplay } = hiddenElements.pop();
   element.style.display = prevDisplay;
 }
@@ -74,11 +94,9 @@ function redisplayPrevItem() {
 // toggle hiding on and off
 function setHiding(shouldHide) {
   if (shouldHide) {
-    document.addEventListener("mousedown", hide, { capture: true });
     document.body.style.cursor = "pointer";
     startHighlighting();
   } else {
-    document.removeEventListener("mousedown", hide, { capture: true });
     document.body.style.cursor = "";
     stopHighlighting();
   }
